0be49bbc75a9b9b211f02fd17aa11e39d4b0576b
Sokojoe <sokojoey@gmail.com>
2018-02-24 01:12:13 -0500

    added search button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class SearchButton extends Component {
+
+  render(){
+    return(
+       <button type="button" onClick={this.handleClick()}> Search Again</button>
+    )
+  }
+
+  handleClick(e){
+    console.log("Button Clicked");
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {new SearchButton().render()}
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
