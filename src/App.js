5656f6f912bea78099946fe14eee659a61576deb
Raya Farhadi <raya.farhadi@gmail.com>
2018-02-24 10:34:03 -0500

    Got country data from api request

--- src/App.js
+++ src/App.js
@@ -16,8 +16,12 @@ class SearchButton extends Component {
 }
 
 class App extends Component {
-
   render() {
+
+    var countryPromise;
+    this.setCountryList();
+    //this.getRandomCountry();
+
     return (
       <div className="App">
         <header className="App-header">
@@ -31,6 +35,33 @@ class App extends Component {
   setBackground() {
     document.body.style.backgroundImage = "url(\"https://farm3.staticflickr.com/2950/33451394876_5b94edcd1c_o.jpg\")";
   }
-}
 
+  setCountryList(){
+    var xhr = new XMLHttpRequest();
+    var list;
+    var countryList;
+    var promise =
+      new Promise(function(resolve, reject){
+          xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+          xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300){
+              resolve(
+              console.log(countryList = JSON.parse(xhr.response))
+            );
+            }
+          };
+          xhr.send();
+        }
+      ).then(function(res) {
+
+      });
+  }
+
+  getRandomCountry(countryList){
+    var numCountries = 250;
+    var countryID = Math.floor(Math.random() * numCountries);
+    console.log(countryList["0"]["name"]);
+  }
+
+}
 export default App;
