05d0ad565e87494f136faf98d3745a575c8d0d86
WesleyMa2 <wesleykhma@gmail.com>
2018-02-24 18:18:49 -0500

    Fixed promises so they are chained

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       source: "empty",
       time: "empty",
       population: 300000,
-      subregion: "North America",
+      subregion: "North America"
     };
     this.countryPromise = this.setCountryList();
   }
@@ -46,7 +46,8 @@ class App extends Component {
         <div className="row">
           <div className="col-md-offset-0 col-md-12 info location">
             <header className="App-header">
-                <h1><img className="flag" src={this.state.flag}></img>{this.state.location}</h1>
+              <h1>
+                <img className="flag" src={this.state.flag}></img>{this.state.location}</h1>
             </header>
           </div>
         </div>
@@ -73,60 +74,55 @@ class App extends Component {
   }
 
   clickButton() {
+    var newState = this.state;
     this.getRandomCountry().then((res) => {
       return this.queryImage(res)
     }).then((photos) => {
       console.log(photos)
       var photoinfo;
-      if (photos){
+      if (photos) {
         photoinfo = photos['photos']['photo'][0];
-      }
-      else{
-         photoinfo = {'url_o':"c1.staticflickr.com/1/66/203162060_5e9db072ab_b.jpg"}
+      } else {
+        photoinfo = {
+          'url_o': "c1.staticflickr.com/1/66/203162060_5e9db072ab_b.jpg"
+        }
       }
       var url;
-        if (photoinfo['url_o']) {
-          console.log(photoinfo)
-          console.log(url = photoinfo['url_o'])
-        } else {
-          url = "https://farm" + photoinfo['farm'] + ".staticflickr.com/" + photoinfo['server'] + "/" + photoinfo['id'] + "_" + photoinfo['secret'] + "_b.jpg"
-        }
-
-      this.queryImageLocation(photoinfo['id']).then((res)=>{
-        console.log(res)
-        var locationJSON = res["photo"]["location"];
-        if (locationJSON["country"]){
-          var country = locationJSON["country"]["_content"];
-        }
-        var place;
-        if (locationJSON["region"]){
-          place = locationJSON["region"]["_content"] + ", ";
-        } else if (locationJSON["locality"]){
-          place = ", " + locationJSON["locality"]["_content"] + ", ";
-        } else if (locationJSON["county"]){
-          place = ", " + locationJSON["county"]["_content"] + ", ";
-        } else {
-          place = ""
-        }
-        var title = place + country;
-        console.log(title);
-        this.setLocation(title);
-      })
-      this.setBackground(url);
-
+      if (photoinfo['url_o']) {
+        console.log(photoinfo)
+        console.log(url = photoinfo['url_o'])
+      } else {
+        url = "https://farm" + photoinfo['farm'] + ".staticflickr.com/" + photoinfo['server'] + "/" + photoinfo['id'] + "_" + photoinfo['secret'] + "_b.jpg"
+      }
+      newState.bgUrl = url;
+      return photoinfo;
+    }).then((photoinfo) => {
+      return this.queryImageLocation(photoinfo['id']);
+    }).then((res) => {
+      console.log(res)
+      var locationJSON = res["photo"]["location"];
+      var country = this.state.countryName;
+      var place = "";
+      if (locationJSON["country"]) {
+        country = locationJSON["country"]["_content"];
+      }
+      if (locationJSON["region"]) {
+        place = locationJSON["region"]["_content"] + ", ";
+      } else if (locationJSON["locality"]) {
+        place = ", " + locationJSON["locality"]["_content"] + ", ";
+      } else if (locationJSON["county"]) {
+        place = ", " + locationJSON["county"]["_content"] + ", ";
+      }
+      var title = place + country;
+      console.log(title);
+      newState.location = title;
+      //this.setLocation(title);
+    }).then(() => {
+      this.setState(newState)
     });
-  }
 
-  setBackground(url) {
-    var newState = this.state;
-    newState.bgUrl = url;
-    this.setState(newState);
-  }
+    //this.setBackground(url);
 
-  setLocation(title) {
-    var newState = this.state;
-    newState.location = title;
-    this.setState(newState);
   }
 
   setCountryList() {
@@ -152,9 +148,9 @@ class App extends Component {
       var newState = this.state;
       this.countryPromise.then((countryList) => {
         newState.countryName = countryList[countryID]["name"]
-        newState.location = newState.countryName;
         newState.population = countryList[countryID]['population'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         newState.flag = countryList[countryID]['flag']
+        //this.setState(newState);
         resolve(newState.countryName);
       })
     })
@@ -181,6 +177,23 @@ class App extends Component {
   queryImageLocation(id) {
     var xhr = new XMLHttpRequest();
     var query = "https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=f60a9176ead0ea9e3cf7d70b0a4353c7&photo_id=" + id + "&format=json&nojsoncallback=1";
+    var promise = new Promise(function(resolve, reject) {
+      console.log(query);
+      xhr.open("GET", query);
+      xhr.onload = function() {
+        if (this.status >= 200 && this.status < 300) {
+          var json = JSON.parse(xhr.response)
+          resolve(json)
+        };
+      }
+      xhr.send();
+    });
+    return promise;
+  }
+
+  getWeatherInfo(lat, lon) {
+    var xhr = new XMLHttpRequest();
+    var query = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=a9393c4dd5430be62c906aad2a95965b"
     var promise = new Promise(function(resolve, reject) {
       console.log(query);
       xhr.open("GET", query);
